58dd37f6b5856e4553a181d9ff3c166c48b33c19
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2022-12-16 20:47:04 -0300

    importando chaves

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import { useEffect } from "react";
+import {id,segredo} from "./componentes/chave";
 
 function App() {
+const [accessToken, setAccessToken] = useState("");
+
 useEffect(()=>{
   let authParameters = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
-    }}
+    },
+    body: 'grant_type=client_credentials&client_id=' + id + '&client_secret=' + segredo
+  }
   fetch('https://accounts.spotify.com/api/token', authParameters)
     .then(result => result.json())
-    .then(data =>console.log(data))      
-}, []);
+    .then(data =>setAccessToken(data.access_token))      
+}, [])
 
 
   return (
